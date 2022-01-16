075906a5fabf7ebee2d1cdc38bc5420309ce4abf
Rittmann <willirittmann@outlook.com>
2022-01-15 21:46:49 -0300

    #Feat: Created a layout to the CustomLabelValueText.kt, this way I can use the Robbie style without any problem. I was getting an error when I was trying to set the TextView style programmatically.

--- common/src/main/java/com/rittmann/common/customs/CustomLabelValueText.kt
+++ common/src/main/java/com/rittmann/common/customs/CustomLabelValueText.kt
@@ -2,27 +2,27 @@ package com.rittmann.common.customs
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.content.withStyledAttributes
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
 import com.rittmann.common.R
 
 @BindingAdapter("valueText")
 fun CustomLabelValueText.setValueText(value: String?) {
-    this.textViewValue.text = value.orEmpty()
+    setValue(value.orEmpty())
     if (value != tag) { // Store the original value
         tag = value   // To prevent duplicate/extra modification
-        this.textViewValue.text = value.orEmpty()
+        setValue(value.orEmpty())
     }
 }
 
 @InverseBindingAdapter(attribute = "valueText")
 fun CustomLabelValueText.getValueText(): String {
-    return this.textViewValue.text.toString()
+    return getValue()
 }
 
 class CustomLabelValueText @JvmOverloads constructor(
@@ -31,11 +31,10 @@ class CustomLabelValueText @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private var textViewLabel: TextView
-    var textViewValue: TextView
-
     private var label = ""
     private var value = ""
+
+    private var valueTextView: TextView
 
     init {
         isClickable = true
@@ -48,57 +47,19 @@ class CustomLabelValueText @JvmOverloads constructor(
             value = getString(R.styleable.CustomLabelValueText_valueText).toString()
         }
 
-        textViewLabel = TextView(context).apply {
-            text = label
-            id = R.id.value_label_custom_label_id
-            addView(this)
+        val customLayout =
+            LayoutInflater.from(context).inflate(R.layout.custom_label_value, this, false)
+        addView(customLayout)
 
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(this@CustomLabelValueText)
-            constraintSet.connect(
-                this.id,
-                ConstraintSet.START,
-                this@CustomLabelValueText.id,
-                ConstraintSet.START,
-                0
-            )
-            constraintSet.connect(
-                this.id,
-                ConstraintSet.TOP,
-                this@CustomLabelValueText.id,
-                ConstraintSet.TOP,
-                0
-            )
-            constraintSet.applyTo(this@CustomLabelValueText)
-        }
+        valueTextView = findViewById(R.id.value_label_custom_value_id)
+        valueTextView.text = value
 
-        textViewValue = TextView(context).apply {
-            text = value
-            id = R.id.value_label_custom_value_id
-            addView(this)
+        findViewById<TextView>(R.id.value_label_custom_label_id).text = label
+    }
 
-            val constraintSet = ConstraintSet()
-            constraintSet.clone(this@CustomLabelValueText)
-            constraintSet.connect(
-                this@apply.id,
-                ConstraintSet.END,
-                this@CustomLabelValueText.id,
-                ConstraintSet.END,
-                0
-            )
-            constraintSet.connect(
-                this@apply.id,
-                ConstraintSet.TOP,
-                this@CustomLabelValueText.id,
-                ConstraintSet.TOP,
-                0
-            )
-            constraintSet.applyTo(this@CustomLabelValueText)
-        }
+    fun getValue() = valueTextView.text.toString()
 
-        val params =
-            MarginLayoutParams(MarginLayoutParams.MATCH_PARENT, MarginLayoutParams.WRAP_CONTENT)
-        setPadding(0, 22, 0, 0)
-        layoutParams = params
+    fun setValue(value: String) {
+        valueTextView.text = value
     }
 }
